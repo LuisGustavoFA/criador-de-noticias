00332e16e628f6197dd470c1585b001da39386ff
LuisGustavoFA <135469560+LuisGustavoFA@users.noreply.github.com>
2023-07-25 21:22:27 -0300

    atualizando página inicial do projeto

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,41 @@
+const alerta = document.getElementById("alerta");
+
 function criaNoticia() {
   const urlBanner = document.getElementById("entrada_banner").value;
   const tituloNoticia = document.getElementById("entrada_titulo").value;
   const textoNoticia = document.getElementById("entrada_texto").value;
   const urlImagem = document.getElementById("entrada_imagem").value;
-  const submit = document.getElementById("entrada_enviar");
+  const entrada = document.getElementById("entrada_enviar");
 
-  window.location.assign(`noticia.html?urlBanner=${urlBanner}&tituloNoticia=${tituloNoticia}&textoNoticia=${textoNoticia}&urlImagem=${urlImagem}`);
+  if (document.getElementById("entrada_banner").value != ""
+    && document.getElementById("entrada_titulo").value != ""
+     && document.getElementById("entrada_texto").value != ""
+      && document.getElementById("entrada_imagem").value != "") {
+
+    window.location.assign(`noticia.html?urlBanner=${urlBanner}&tituloNoticia=${tituloNoticia}&textoNoticia=${textoNoticia}&urlImagem=${urlImagem}`);
+
+  } else {
+    console.error("Todos os inputs devem ser preenchidos.");
+    exibeErro(1200);
+  }
+
+  function exibeErro(duracao) {
+    alerta.style.display = "flex";
+    alerta.style.opacity = 1;
+    desabilitaEntrada(duracao + 250);
+
+    setTimeout(() => {
+      alerta.style.opacity = 0;
+
+      setTimeout(() => {
+        alerta.style.display = "none";
+      }, 250);
+
+    }, duracao);
+  }
+
+  function desabilitaEntrada(duracao) {
+    entrada.disabled = true;
+    setTimeout(() => {entrada.disabled = false}, duracao);
+  }
 }
