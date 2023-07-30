6fc2b1109c8bacfa1332bab71f960b6d7e6d40e4
LuisGustavoFA <135469560+LuisGustavoFA@users.noreply.github.com>
2023-07-30 14:58:11 -0300

    atualizando aparencia

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,16 @@
+const entrada = document.getElementById("entrada_enviar");
 const alerta = document.getElementById("alerta");
+const teste = document.getElementById("teste");
+
+teste.addEventListener("click", ()=>{
+  window.location.assign("noticia.html?urlBanner=TESTE&tituloNoticia=TESTE&textoNoticia=TESTE&urlImagem=TESTE");
+})
 
 function criaNoticia() {
   const urlBanner = document.getElementById("entrada_banner").value;
   const tituloNoticia = document.getElementById("entrada_titulo").value;
   const textoNoticia = document.getElementById("entrada_texto").value;
   const urlImagem = document.getElementById("entrada_imagem").value;
-  const entrada = document.getElementById("entrada_enviar");
 
   if (document.getElementById("entrada_banner").value != ""
     && document.getElementById("entrada_titulo").value != ""
@@ -16,26 +21,26 @@ function criaNoticia() {
 
   } else {
     console.error("Todos os inputs devem ser preenchidos.");
-    exibeErro(1200);
+    exibeErro(1500);
+    desabilitaEntrada(1750);
   }
+}
 
-  function exibeErro(duracao) {
-    alerta.style.display = "flex";
-    alerta.style.opacity = 1;
-    desabilitaEntrada(duracao + 250);
+function exibeErro(duracao) {
+  alerta.style.display = "flex";
+  alerta.style.opacity = 1;
+
+  setTimeout(() => {
+    alerta.style.opacity = 0;
 
     setTimeout(() => {
-      alerta.style.opacity = 0;
+      alerta.style.display = "none";
+    }, 250);
 
-      setTimeout(() => {
-        alerta.style.display = "none";
-      }, 250);
+  }, duracao);
+}
 
-    }, duracao);
-  }
-
-  function desabilitaEntrada(duracao) {
-    entrada.disabled = true;
-    setTimeout(() => {entrada.disabled = false}, duracao);
-  }
+function desabilitaEntrada(duracao) {
+  entrada.disabled = true;
+  setTimeout(() => {entrada.disabled = false}, duracao);
 }
--- scripts/noticia.js
+++ scripts/noticia.js
@@ -1,6 +1,9 @@
 const urlParams = new URLSearchParams(window.location.search);
 
-document.querySelector("header").style.backgroundImage = `url(${urlParams.get("urlBanner")})`;
 document.querySelector(".conteudo_titulo").innerHTML = urlParams.get("tituloNoticia");
 document.querySelector(".conteudo_texto").innerHTML = urlParams.get("textoNoticia");
-document.querySelector(".conteudo_imagem").src = urlParams.get("urlImagem");
+
+if (urlParams.get("urlImagem").length > 9 || urlParams.get("urlBanner").length > 9 ) {
+  document.querySelector("header").style.backgroundImage = `url(${urlParams.get("urlBanner")})`;
+  document.querySelector(".conteudo_imagem").src = urlParams.get("urlImagem");
+}
